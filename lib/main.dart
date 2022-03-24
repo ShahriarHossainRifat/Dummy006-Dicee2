2a4e947b552e37e37b4c86e9ac6213d1b5cda593
Shahriar Rifat <shahriarrifat0@gmail.com>
2022-03-24 12:33:41 +0600

    added elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
-      backgroundColor: Colors.grey[800],
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        backgroundColor: Colors.grey[900],
+        backgroundColor: Colors.deepOrange[600],
         title: Text('Dicee 2'),
         centerTitle: true,
       ),
@@ -23,8 +24,46 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void randomizeDice() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+                child: Image.asset('images/dice$leftDice.png'),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(10, 20, 20, 20),
+                child: Image.asset('images/dice$rightDice.png'),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 15.0),
+        ElevatedButton(
+            child: Text('Randomize'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.deepOrange[600],
+            ),
+            onPressed: () {
+              randomizeDice();
+            }),
+      ],
+    );
   }
 }
